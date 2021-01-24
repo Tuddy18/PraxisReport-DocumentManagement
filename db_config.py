0987b5eb88451ddc56c201e786dce50525f67096
tuddy <tuddy18_26@yahoo.com>
2021-01-24 16:26:12 +0200

    initial commit

---
+++ db_config.py
@@ -0,0 +1,10 @@
+# from app import app
+from flask_mysqldb import MySQL
+import os
+config = {}
+
+config['MYSQL_HOST'] = 'remotemysql.com'
+config['MYSQL_USER'] = 'WjXojylojp'
+config['MYSQL_PASSWORD'] = 'AtlTrhHwfZ'
+config['MYSQL_DB'] = 'WjXojylojp'
+config['MYSQL_CURSORCLASS'] = 'DictCursor'
