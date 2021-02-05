9e41059d74edc80b26c8cf599c4858f1d83e22a0
tuddy <tuddy18_26@yahoo.com>
2021-02-05 15:06:03 +0200

    fixed form type

--- app/service/praxis_service.py
+++ app/service/praxis_service.py
@@ -80,7 +80,19 @@ def get_form_by_email():
         resp = jsonify(success=False, message='praxis not found')
         resp.status_code = 404
         return resp
-    resp = jsonify(form.json_dict())
+
+    form_json = form.json_dict()
+    if praxis.student_form.email == email:
+        form_json['type'] = 'student_form'
+    elif praxis.mentor_form.email == email:
+        form_json['type'] = 'mentor_form'
+    elif praxis.professor_form == email:
+        form_json['type'] = 'professor_form'
+    else:
+        resp = jsonify(success=False, message='praxis not found')
+        resp.status_code = 404
+        return resp
+    resp = jsonify(form_json)
     return resp
 
 @app.route('/praxis/get-by-email', methods=['POST'])
